a27499acc1b6e0abddfc96c17ff9bf7a16a3138d
SpencerPearson <spencer.pearson.816@outlook.com>
2022-07-21 14:02:50 -0500

    changed stream to AzuraCast player

--- src/Components/LiveStream.js
+++ src/Components/LiveStream.js
@@ -1,30 +1,74 @@
-import React from "react"
-import ReactAudioPlayer from "react-audio-player"
+import React from 'react'
 import Marquee from 'react-fast-marquee'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCannabis } from '@fortawesome/free-solid-svg-icons'
 import './LiveStream.css'
 
 export default function LiveStream(props) {
-	return (
-		<section className='liveHeader'>
-        <div className="liveStrip">
-            <Marquee gradient={false} speed={50}>
-                <p className="liveMarquee">LISTEN LIVE &ensp;<FontAwesomeIcon icon={faCannabis} />&emsp;</p>
-                <p className="liveMarquee">LISTEN LIVE &ensp;<FontAwesomeIcon icon={faCannabis} />&emsp;</p>
-                <p className="liveMarquee">LISTEN LIVE &ensp;<FontAwesomeIcon icon={faCannabis} />&emsp;</p>
-                <p className="liveMarquee">LISTEN LIVE &ensp;<FontAwesomeIcon icon={faCannabis} />&emsp;</p>
-                <p className="liveMarquee">LISTEN LIVE &ensp;<FontAwesomeIcon icon={faCannabis} />&emsp;</p>
-                <p className="liveMarquee">LISTEN LIVE &ensp;<FontAwesomeIcon icon={faCannabis} />&emsp;</p>
-                <p className="liveMarquee">LISTEN LIVE &ensp;<FontAwesomeIcon icon={faCannabis} />&emsp;</p>
-                <p className="liveMarquee">LISTEN LIVE &ensp;<FontAwesomeIcon icon={faCannabis} />&emsp;</p>
-                <p className="liveMarquee">LISTEN LIVE &ensp;<FontAwesomeIcon icon={faCannabis} />&emsp;</p>
-                <p className="liveMarquee">LISTEN LIVE &ensp;<FontAwesomeIcon icon={faCannabis} />&emsp;</p>
-            </Marquee>
-		</div>
-        <div className='player'>
-            <ReactAudioPlayer src='https://stream.bowlafterbowl.com/listen/bowlafterbowl/stream.mp3' controls />
-        </div>
-        </section>
-	)
+  return (
+    <section className='liveHeader'>
+      <div className='liveStrip'>
+        <Marquee gradient={false} speed={50}>
+          <p className='liveMarquee'>
+            LISTEN LIVE &ensp;
+            <FontAwesomeIcon icon={faCannabis} />
+            &emsp;
+          </p>
+          <p className='liveMarquee'>
+            LISTEN LIVE &ensp;
+            <FontAwesomeIcon icon={faCannabis} />
+            &emsp;
+          </p>
+          <p className='liveMarquee'>
+            LISTEN LIVE &ensp;
+            <FontAwesomeIcon icon={faCannabis} />
+            &emsp;
+          </p>
+          <p className='liveMarquee'>
+            LISTEN LIVE &ensp;
+            <FontAwesomeIcon icon={faCannabis} />
+            &emsp;
+          </p>
+          <p className='liveMarquee'>
+            LISTEN LIVE &ensp;
+            <FontAwesomeIcon icon={faCannabis} />
+            &emsp;
+          </p>
+          <p className='liveMarquee'>
+            LISTEN LIVE &ensp;
+            <FontAwesomeIcon icon={faCannabis} />
+            &emsp;
+          </p>
+          <p className='liveMarquee'>
+            LISTEN LIVE &ensp;
+            <FontAwesomeIcon icon={faCannabis} />
+            &emsp;
+          </p>
+          <p className='liveMarquee'>
+            LISTEN LIVE &ensp;
+            <FontAwesomeIcon icon={faCannabis} />
+            &emsp;
+          </p>
+          <p className='liveMarquee'>
+            LISTEN LIVE &ensp;
+            <FontAwesomeIcon icon={faCannabis} />
+            &emsp;
+          </p>
+          <p className='liveMarquee'>
+            LISTEN LIVE &ensp;
+            <FontAwesomeIcon icon={faCannabis} />
+            &emsp;
+          </p>
+        </Marquee>
+      </div>
+      <div className='player text-center'>
+        <iframe
+          title='AzuraCast stream'
+          src='https://stream.bowlafterbowl.com/public/bowlafterbowl/embed?theme=dark'
+          frameborder='0'
+          allowtransparency='true'
+          style={{ maxWidth: '100%', minHeight: '150px', border: 0, margin: 'auto' }}></iframe>
+      </div>
+    </section>
+  )
 }
